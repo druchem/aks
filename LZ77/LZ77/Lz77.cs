90ee4774b20d2e11cfbce384ef5c73ae23c29c3b
Druchem <mrdruchem@gmail.com>
2019-03-19 22:11:17 +0100

    Added incomplete LZ77 compression code.

---
+++ LZ77/LZ77/Lz77.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LZ77
+{
+	internal struct Triplet
+	{
+		public int MatchOffset { get; set; }
+		public int MatchLength { get; set; }
+		public byte NonMatchedByte { get; set; }
+
+		public override string ToString() => $"({MatchOffset}, {MatchLength}, {NonMatchedByte})";
+	}
+
+	internal class Lz77
+	{
+		private readonly int _windowSize;
+		private readonly int _maxMatchSize;
+		private readonly int _maxSearchBufferSize;
+
+		public Lz77(int windowSize, int maxMatchSize)
+		{
+			_windowSize = windowSize;
+			_maxMatchSize = maxMatchSize;
+			_maxSearchBufferSize = windowSize - maxMatchSize;
+		}
+
+		public List<Triplet> Encode(byte[] input)
+		{
+			Span<byte> buffer = input;
+
+			var triplets = new List<Triplet>();
+			int lookAheadIndex = 0;
+			int searchOffset = 0;
+
+			while (lookAheadIndex + _maxMatchSize < buffer.Length)
+			{
+				var lookAheadBuffer = buffer.Slice(lookAheadIndex, _maxMatchSize);
+
+				int searchBufferSize = lookAheadIndex < _maxSearchBufferSize ? lookAheadIndex : _maxSearchBufferSize;
+				var searchBuffer = buffer.Slice(searchOffset, searchBufferSize);
+
+				var triplet = GetTriplet(searchBuffer, lookAheadBuffer);
+				triplets.Add(triplet);
+
+				lookAheadIndex += triplet.MatchLength + 1;
+
+				if (lookAheadIndex > _maxSearchBufferSize)
+					searchOffset = lookAheadIndex - _maxSearchBufferSize;
+			}
+
+			return triplets;
+		}
+
+		private Triplet GetTriplet(Span<byte> searchBuffer, Span<byte> lookAheadBuffer)
+		{
+			var triplet = new Triplet
+			{
+				MatchLength = 0,
+				MatchOffset = 0,
+				NonMatchedByte = lookAheadBuffer[0]
+			};
+
+			for (int matchLength = 1; matchLength < lookAheadBuffer.Length; matchLength++)
+			{
+				var searchPattern = lookAheadBuffer.Slice(0, matchLength);
+
+				int matchIndex = searchBuffer.LastIndexOf(searchPattern);
+				if (matchIndex == -1)
+					break;
+
+				triplet = new Triplet
+				{
+					MatchLength = matchLength,
+					MatchOffset = searchBuffer.Length - matchIndex,
+					NonMatchedByte = lookAheadBuffer[matchLength] // can crash
+				};
+			}
+
+			return triplet;
+		}
+	}
+}
---
+++ LZ77/LZ77/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace LZ77
+{
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			var files = new[] {"czech.txt", "english.txt", "french.txt", "german.txt", "hungarian.txt"};
+			var parameters = new(int WindowSize, int MaxMatch)[]  {(4096, 16), (16384, 32), (32768, 64)};
+
+			Console.WriteLine("File\t\tTriplets\tFileSize\tWindowSize\tMax.Match\tEnc.Size\tbps");
+			foreach (string file in files)
+			{
+				Console.WriteLine("-----------------------------------------------------------------------------------------------------");
+
+				var input = File.ReadAllBytes(file);
+				foreach (var parameter in parameters)
+				{
+					var lz = new Lz77(parameter.WindowSize, parameter.MaxMatch);
+					var triplets = lz.Encode(input);
+
+					int length = triplets.Sum(triplet => triplet.MatchLength);
+
+					Console.WriteLine($"{file}\t{triplets.Count}\t\t{input.Length}\t\t{parameter.WindowSize}\t\t{parameter.MaxMatch}\t\t{length}\t\t?????");
+				}
+			}
+
+			Console.ReadLine();
+		}
+	}
+}
